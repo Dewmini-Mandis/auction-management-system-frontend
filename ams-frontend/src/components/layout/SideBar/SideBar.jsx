16a6f76c358999780c6d232df852e4852ac4839e
Dewmini-Mandis <136433945+Dewmini-Mandis@users.noreply.github.com>
2024-09-23 09:45:53 +0530

    update headerSeller and sidebar

--- ams-frontend/src/components/layout/SideBar/SideBar.jsx
+++ ams-frontend/src/components/layout/SideBar/SideBar.jsx
@@ -5,7 +5,8 @@ import { Link } from 'react-router-dom';
 import expandArrow from '../../../../src/assets/images/expandArrow.png';
 import contractArrow from '../../../../src/assets/images/contractArrow.png';
 import logo from '../../../assets/images/logo.png';
-function SideBar() {
+
+function SideBar({ isSidebarVisible }) {
     const [openMenu, setOpenMenu] = useState({});
 
     const toggleMenu = (menuName) => {
@@ -13,26 +14,28 @@ function SideBar() {
           ...prevOpenMenu,
           [menuName]: !prevOpenMenu[menuName],
         }));
+
         
       };
-  return (<div className="w-1/6 h-full bg-[#F5F0FA] shadow-md">
+  return (
+  <div className={`2xl:pl-6 w-1/3 md:w-1/6 h-full bg-[#FDFAFF] shadow-md md:block ${isSidebarVisible ? 'block' : 'hidden'} `}>
 
-    <div className='w-full h-fit'>
-        <img src={logo} alt="logo" className='h-10 m-4 w-55' />
+    <div className='flex items-center justify-center w-full h-fit'>
+        <img src={logo} alt="logo" className='h-5 m-1 mt-2 w-25 xss:h-6 xss:w-30 md:m-3 lg:m-4 md:w-35 md:h-7 xl:h-10 xl:w-55' />
     </div>
-  <ul className="p-4">
+  <ul className="p-3 xss:p-4 lg:px-6 xl:px-8 ">
     <li className="py-2 border-solid border-y border-neutral-200 ">
       <div
         onClick={() => toggleMenu('products')}
         className="flex justify-between cursor-pointer"
       >
-        <span className='text-sm text-gray-600 '>Products</span>
+          <span className={`text-[10px] md:text-[12px] lg:text-[13px] xl:text-sm ${openMenu['products'] ? 'font-medium' : ''} `}>Products</span>
         <span>
-        <img src={openMenu['products'] ? expandArrow : contractArrow} alt="toggle" className='w-4 h-4'/>
+        <img src={openMenu['products'] ? expandArrow : contractArrow} alt="toggle" className='w-3 h-3 lg:w-4 lg:h-4 '/>
         </span>
       </div>
       {openMenu['products'] && (
-        <ul className="pl-4 text-sm text-gray-600">
+        <ul className="pl-4 text-[10px] md:text-[12px] lg:text-[13px] xl:text-sm text-gray-600 active:text-[#5f288f] active:font-medium">
           <li className="py-1">
           <Link to='/products/list'>Products list</Link>
           </li>
@@ -47,13 +50,13 @@ function SideBar() {
     onClick={() => toggleMenu('auctions')}
     className="flex justify-between cursor-pointer"
   >
-    <span className='text-sm text-gray-600 '><Link to='/auctions'>Auctions</Link></span>
+    <span className={`text-[10px] md:text-[12px] lg:text-[13px] xl:text-sm ${openMenu['auctions'] ? 'font-medium' : ''}`}><Link to='/auctions'>Auctions</Link></span>
     <span>
-    <img src={openMenu['auctions']? expandArrow : contractArrow} alt="toggle" className='w-4 h-4' />
+    <img src={openMenu['auctions']? expandArrow : contractArrow} alt="toggle" className='w-3 h-3 lg:w-4 lg:h-4' />
     </span>
   </div>
   {openMenu['auctions'] && (
-    <ul className="pl-4 text-sm text-gray-600">
+    <ul className="pl-4 text-[10px] md:text-[12px] lg:text-[13px] xl:text-sm text-gray-600 active:text-[#5f288f] active:font-medium">
       <li className="py-1">
       <Link to='/auctions/active'>Active</Link>
       </li>
@@ -68,13 +71,13 @@ function SideBar() {
     onClick={() => toggleMenu('notifications')}
     className="flex justify-between cursor-pointer"
   >
-    <span className='text-sm text-gray-600 '> <Link to='/notifications'>Notifications</Link></span>
+    <span className={`text-[10px] md:text-[12px] lg:text-[13px] xl:text-sm ${openMenu['notifications'] ? 'font-medium' : ''}`}> <Link to='/notifications'>Notifications</Link></span>
     <span>
-    <img src={openMenu['notifications']? expandArrow : contractArrow} alt="toggle" className='w-4 h-4'/>
+    <img src={openMenu['notifications']? expandArrow : contractArrow} alt="toggle" className='w-3 h-3 lg:w-4 lg:h-4'/>
     </span>
   </div>
   {openMenu['notifications'] && (
-    <ul className="pl-4 text-sm text-gray-600">
+    <ul className="pl-4 text-[10px] md:text-[12px] lg:text-[13px] xl:text-sm text-gray-600 active:text-[#5f288f] active:font-medium">
       <li className="py-1">
       <Link to='/notifications/readed'>Readed</Link>
       </li>
@@ -89,13 +92,13 @@ function SideBar() {
     onClick={() => toggleMenu('categories')}
     className="flex justify-between cursor-pointer"
   >
-    <span className='text-sm text-gray-600 '>Categories</span>
+    <span className={`text-[10px] md:text-[12px] lg:text-[13px] xl:text-sm ${openMenu['categories'] ? 'font-medium' : ''}`}>Categories</span>
     <span>
-    <img src={openMenu['Categories']? expandArrow : contractArrow} alt="toggle" className='w-4 h-4' />
+    <img src={openMenu['Categories']? expandArrow : contractArrow} alt="toggle" className='w-3 h-3 lg:w-4 lg:h-4' />
    </span>
   </div>
   {openMenu['categories'] && (
-    <ul className="pl-4 text-sm text-gray-600">
+    <ul className="pl-4 text-[10px] md:text-[12px] lg:text-[13px] xl:text-sm text-gray-600 active:text-[#5f288f] hover:font-medium">
       <li className="py-1">
       <Link to='/category/list'>Category list</Link>
       </li>
@@ -110,13 +113,13 @@ function SideBar() {
     onClick={() => toggleMenu('labels')}
     className="flex justify-between cursor-pointer"
   >
-    <span className='text-sm text-gray-600 '>Labels</span>
+    <span className={`text-[10px] md:text-[12px] lg:text-[13px] xl:text-sm ${openMenu['categories'] ? 'font-medium' : ''} `}>Labels</span>
     <span>
-    <img src={openMenu['labels']? expandArrow : contractArrow} alt="toggle" className='w-4 h-4'/>
+    <img src={openMenu['labels']? expandArrow : contractArrow} alt="toggle" className='w-3 h-3 lg:w-4 lg:h-4'/>
     </span>
   </div>
   {openMenu['labels'] && (
-    <ul className="pl-4 text-sm text-gray-600">
+    <ul className="pl-4 text-[10px] md:text-[12px] lg:text-[13px] xl:text-sm text-gray-600 active:text-[#5f288f] active:font-medium">
       <li className="py-1">
       <Link to='/labels/list'>Label list</Link>
       </li>
@@ -128,7 +131,7 @@ function SideBar() {
 </li>
 <li className="py-2 border-solid border-y border-neutral-200">
  
-    <span className='text-sm text-gray-600 '><Link to='/bids'>Bids</Link></span>
+    <span className='text-[10px] md:text-[12px] lg:text-[13px] xl:text-sm text-gray-600 '><Link to='/bids'>Bids</Link></span>
     
 </li>
   </ul>
---
+++ ams-frontend/src/pages/Product/ProductList/ProductList.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useState } from 'react';
+import SideBar from '../../../components/layout/SideBar/SideBar';
+import HeaderSeller from '../../../components/layout/HeaderSeller/HeaderSeller';
+function ProductList() {
+
+    const [isSidebarVisible, setIsSidebarVisible] = useState(false);
+
+    const toggleSidebarVisibility = () => {
+        setIsSidebarVisible(!isSidebarVisible);
+      };
+      
+   
+  return (
+    <div className='w-full h-screen parent-container' >
+            <React.Fragment>
+                <div className="flex w-full h-full">
+
+                <SideBar isSidebarVisible={isSidebarVisible} />
+
+                    <div className={`flex flex-col md:w-5/6 bg-[#F5F0FA] ${isSidebarVisible ? 'w-3/4' : 'w-full'}`}>
+                    <HeaderSeller toggleSidebarVisibility={toggleSidebarVisibility} isSidebarVisible={isSidebarVisible}/>
+
+                        <div className="flex-grow p-4 text-black border border-solid border-neutral-200">
+                            <p>Welcome to the seller dashboard!</p>
+                        </div>
+
+                    </div>
+                </div>
+               
+            </React.Fragment>
+        </div>
+  )
+}
+
+export default ProductList
