1a89060f25e526ff680464991891548093e3acda
Kasun-Abeywickrama <kasunthiwanka158@gmail.com>
2024-10-27 16:34:37 +0530

    Resolve conflicts

--- ams-frontend/src/routes.jsx
+++ ams-frontend/src/routes.jsx
@@ -4,12 +4,19 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // Import your components
 import HomePage from './pages/Home/HomePage';
 import NotFound from './components/NotFound/NotFound';
-import ProductList from './pages/Product/ProductList/ProductList';
-import CategoryList from './pages/Category_list/Category';
-import CategoryProduct from './pages/Category_list/Categoryproduct';
-import Subcategory from './pages/Category_list/Subcategory';
-import Watchlist from './pages/Watchlist/Watchlist';
+import Notification from './pages/Notification/Notification';
+import Slidebar from './pages/Slidebar/Slidebar';
 
+import ProductList from './pages/Product/ProductList/ProductList';
+import MyBids from './pages/MyBids/MyBids';
+import WishList from './pages/Wishlist/WishList';
+import Product from './pages/Product/ProductList/Product';
+
+import SignInPage from './pages/Auth/SignIn/SignInPage';
+import SignUpPage from './pages/Auth/SignUp/SignUpPage';
+import EmailVerification from './pages/Auth/EmailVerification/EmailVerification';
+import ForgotPassword from './pages/Auth/ForgotPassword/ForgotPassword';
+import ResetPassword from './pages/Auth/ResetPassword/ResetPassword';
 
 const AppRoutes = () => {
   return (
@@ -19,19 +26,31 @@ const AppRoutes = () => {
         {/* Public Routes */}
         <Route path="/" element={<HomePage />} />
         <Route path="/productlist" element={<ProductList />} />
-        <Route path="/categorylist" element={<CategoryList />} />
-        <Route path="/categoryproduct"element={<CategoryProduct />}/>
-        <Route path="/subcategory"element={<Subcategory/>}/>
-        <Route path="/watchlist"element={<Watchlist/>}/>
+        <Route path="/wishlist" element={<WishList />} />
+        <Route path="/mybids" element={<MyBids />} />
+        <Route path="/product" element={<Product />} />
 
         {/* Auth Routes */}
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/email-verification" element={<EmailVerification />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
 
+        
 
         {/* Protected Routes */}
 
 
         {/* Catch-All Route */}
         <Route path="*" element={<NotFound />} />
+
+        <Route path="/notification" element={<Notification/>} />
+
+        <Route path="/slidebar" element={<Slidebar/>} />
+
+       
+        
 
       </Routes>
     </Router>
