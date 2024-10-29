ad4f85b8819a853b42ae7c0cf7760e98a1796381
Kasun-Abeywickrama <kasunthiwanka158@gmail.com>
2024-10-30 01:47:17 +0530

    Update CategoryCard.jsx

--- ams-frontend/src/components/Cards/CategoryCard.jsx
+++ ams-frontend/src/components/Cards/CategoryCard.jsx
@@ -4,13 +4,13 @@ import CountdownTimer from "./CountdownTimer";
 const CategoryCard = ({ category }) => {
 
     const categoryImageUrl =
-        category?.imageUrls?.[0]
-            ? import.meta.env.VITE_BASE_URL + auction.imageUrls?.[0]
+        category?.logoImageUrls?.[0]
+            ? import.meta.env.VITE_BASE_URL + category.logoImageUrls?.[0]
             : "https://via.placeholder.com/150"; // Fallback image URL
 
     return (
         <div key={category.categoryId} className="p-4 rounded-lg cursor-pointer">
-            <img src="https://via.placeholder.com/150" alt="Heart" className="w-full rounded-full" />
+            <img src={categoryImageUrl} alt="Heart" className="w-full rounded-full" />
             <h1 className="text-center text-xl font-semibold mt-4">{category.name}</h1>
         </div>
     );
